3068d00d72d2a09bfe6fa6d75136e6a55a6feb2e
Vk167 <viijaykumarr07@gmail.com>
2026-07-07 08:41:59 +0530

    terraform vpc test

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "vpc" {
-    source = "../terraform-aws-vpc"
+    # source = "../terraform-aws-vpc"
+    source = "git::https://github.com/vijaykumar-456/terraform-aws-vpc.git?ref=main"
     project = var.project
     environment = var.environment
     is_peering_required = true
